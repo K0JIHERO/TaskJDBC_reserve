4bbbb0d1d02fb5937216f5614770fa1b28421372
k0jihero <guagetru.bla@inbox.ru>
2021-01-19 22:29:30 +0900

    Исправил создание объектов (создал через интерфейсы)

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,39 +1,40 @@
 package jm.task.core.jdbc.service;
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.sql.*;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
+    private UserDao userDao = new UserDaoJDBCImpl();
 
     @Override
     public void createUsersTable() throws SQLException {
-        userDaoJDBCImpl.createUsersTable();
+        userDao.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() throws SQLException {
-        userDaoJDBCImpl.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        userDaoJDBCImpl.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) throws SQLException {
-        userDaoJDBCImpl.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() throws SQLException {
-        return userDaoJDBCImpl.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() throws SQLException {
-        userDaoJDBCImpl.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
